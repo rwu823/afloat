52afff42aea76e4080b587b05382877c9d983664
RoCky Wu <rocky_wu@trend.com.tw>
2015-09-22 14:27:41 +0800

    change to && replace ;

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-wget https://github.com/rwu823/afloat/raw/master/src/installer-afloat.zip;
-unzip ./installer-afloat.zip;
+wget https://github.com/rwu823/afloat/raw/master/src/installer-afloat.zip &&
+
+unzip ./installer-afloat.zip &&
 
 # install SIMBL
-sudo installer -verbose -pkg ./SIMBL-0.9.9.pkg  -target /;
+sudo installer -verbose -pkg ./SIMBL-0.9.9.pkg  -target / &&
 
 # install Afloat.bundle
-mkdir -p ~/Library/Application\ Support/SIMBL/Plugins;
+mkdir -p ~/Library/Application\ Support/SIMBL/Plugins &&
 mv ./Afloat.bundle ~/Library/Application\ Support/SIMBL/Plugins;
