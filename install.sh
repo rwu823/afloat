5a246616b9d0a2ed71b194bf1b19b829c42b3b58
RoCky Wu <rocky_wu@trend.com.tw>
2015-09-29 16:17:10 +0800

    add semicolons

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ mkdir -p ~/Library/Application\ Support/SIMBL/Plugins &&
 mv ./Afloat.bundle ~/Library/Application\ Support/SIMBL/Plugins;
 
 # Clean the working directory after installing afloat.
-rm SIMBL-0.9.9.pkg
-rm installer-afloat.zip
+rm SIMBL-0.9.9.pkg;
+rm installer-afloat.zip;
